2c383bdbf26d8a06a1a824a04efc723b215a1d47
gogu123456 <ramreddygogulamudi@gmail.com>
2025-10-22 15:05:15 +0530

    Add files via upload

---
+++ CONSTRUCTOR.py
@@ -0,0 +1,18 @@
+#we’ll create a Class Student with an instance variable student 
+#name. we’ll see how to use a constructor to initialize the 
+#student name at the time of object creation.
+class std:
+    def __init__(self, name):
+        self.name=name
+    def show(self):
+        print( "my name :",s1.name)
+    def __del__(self):
+        print("object destroued")
+
+s1=std("csit- it II")
+print("\u0c05  \u0c08")
+s1.show()
+del s1
+#print(" class name :",s1.name)
+        
+
